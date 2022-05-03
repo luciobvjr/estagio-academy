6c611b819404bb33e9ec1a7d4bfe60edf2a96bb0
Lucio Bueno Vieira Junior <luciobvjr@gmail.com>
2022-05-03 14:50:33 -0300

    XCode project created

---
+++ 3Dchem/3Dchem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  3Dchem
+//
+//  Created by Lucio Bueno Vieira Junior on 03/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 3Dchem/3Dchem/_DchemApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DchemApp.swift
+//  3Dchem
+//
+//  Created by Lucio Bueno Vieira Junior on 03/05/22.
+//
+
+import SwiftUI
+
+@main
+struct _DchemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
