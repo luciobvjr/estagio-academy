864fa2202f6d921b75ae05f6b8a3d37eaec65919
Lucio Bueno Vieira Junior <luciobvjr@gmail.com>
2022-05-03 16:52:15 -0300

    setup

---
+++ 3Dchem/3Dchem/Views/SubjectView.swift
@@ -0,0 +1,22 @@
+//
+//  SubjectView.swift
+//  3Dchem
+//
+//  Created by Lucio Bueno Vieira Junior on 03/05/22.
+//
+
+import SwiftUI
+
+struct SubjectView: View {
+    var body: some View {
+        NavigationView() {
+            
+        }
+    }
+}
+
+struct SubjectView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubjectView()
+    }
+}
