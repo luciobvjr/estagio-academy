49a027fb516ee80d92e2929e9baf560c0412a8dd
Lucio Bueno Vieira Junior <luciobvjr@gmail.com>
2022-05-03 17:23:06 -0300

    layout done

--- 3Dchem/3Dchem/Views/SubjectView.swift
+++ 3Dchem/3Dchem/Views/SubjectView.swift
@@ -8,15 +8,47 @@
 import SwiftUI
 
 struct SubjectView: View {
+    let title: String
+    let text: String
+    
     var body: some View {
         NavigationView() {
-            
+            ScrollView {
+                VStack {
+                    HStack {
+                        Text(text)
+                            .padding()
+                        Spacer()
+                    }
+                
+                    MyButton()
+                }
+            }
+            .navigationTitle(Text(title))
         }
+    }
+}
+
+struct MyButton: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .frame(height: 46, alignment: .center)
+                .foregroundColor(.black)
+                .cornerRadius(20)
+                .padding([.leading, .trailing])
+            
+            Text("Load 3D model")
+                .foregroundColor(Color.white)
+                .font(.system(size: 20))
+        }
+        .padding(.bottom)
     }
 }
 
 struct SubjectView_Previews: PreviewProvider {
     static var previews: some View {
-        SubjectView()
+        SubjectView(title: "Geometria molecular",
+                    text: "Second line of text in here for this text element or component. Second line of text in here for this text element or component. Second line of text in here for this text element or component. Second line of text in here for this text element or component. Second line of text in here for this text element or component. Second line of text in here for this text element or component. Second line of text in here for this text element or component. Second line of text in here for this text element or component. Second line of text in here for this text element or component. Second line of text in here for this text element or component. Second line of text in here for this text element or component. Second line of text in here for this text element or component. Second line of text in here for this text element or component. Second line of text in here for this text element or component. Second line of text in here for this text element or component. ")
     }
 }
